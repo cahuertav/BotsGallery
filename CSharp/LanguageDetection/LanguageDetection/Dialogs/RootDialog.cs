6862fe570eca4e3f03dd113f30d8d69ba27b29a4
Amin Espinoza <doomsday5@hotmail.com>
2018-06-07 21:34:43 -0500

    Language detection bot in csharp
    My first bot in this gallery! Yujuu!

---
+++ CSharp/LanguageDetection/LanguageDetection/Dialogs/RootDialog.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Net.Http;
+using System.Runtime.Serialization;
+using System.Threading.Tasks;
+using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Bot.Connector;
+
+namespace LanguageDetection.Dialogs
+{
+    [Serializable]
+    public class RootDialog : IDialog<object>
+    {
+        public Task StartAsync(IDialogContext context)
+        {
+            context.Wait(MessageReceivedAsync);
+
+            return Task.CompletedTask;
+        }
+
+        private async Task MessageReceivedAsync(IDialogContext context, IAwaitable<object> result)
+        {
+            var activity = await result as Activity;
+
+            string content = activity.Text;
+            string languageCode = await GetDesiredLanguageAsync(content);
+            context.PostAsync(languageCode);
+        }
+
+        public async static Task<string> GetDesiredLanguageAsync(string content)
+        {
+            HttpClient languageClient = new HttpClient();
+            languageClient.DefaultRequestHeaders.Add("Ocp-Apim-Subscription-Key", "45474d57d06346679d2c66648a76f175");
+            var detectLanguageResponse = await languageClient.GetStreamAsync(
+                $"http://api.microsofttranslator.com/v2/http.svc/Detect?text={content}");
+
+            return (string)new DataContractSerializer(typeof(string)).ReadObject(detectLanguageResponse);
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# BotsGallery
-Gallery of bot samples made for specific scenarios
+# Overview
+The idea of this repository is having a lot of simple sample scenarios you could use in bots so, any time you'd need some new features you can come and take a look about what's new.
+
+### Cognitive samples
+
+|Sample|Description|C#|Node|
+|LanguageDetection|Write a word or a sentence, the bot will give you back the code of that language|ViewSample|In construction|
