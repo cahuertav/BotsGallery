f23c292d8ff9b4acde6e68bd5a2025f61a84e0b2
Amin Espinoza <doomsday5@hotmail.com>
2018-07-13 14:13:12 -0500

    Bot de alfabeto radiofonico agregado

---
+++ CSharp/BotFonetico/BotFonetico/Dialogs/RootDialog.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Threading.Tasks;
+using BotFonetico.Helpers;
+using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Bot.Connector;
+
+namespace BotFonetico.Dialogs
+{
+    [Serializable]
+    public class RootDialog : IDialog<object>
+    {
+        public Task StartAsync(IDialogContext context)
+        {
+            context.Wait(MessageReceivedAsync);
+
+            return Task.CompletedTask;
+        }
+
+        private async Task MessageReceivedAsync(IDialogContext context, IAwaitable<object> result)
+        {
+            var activity = await result as Activity;
+            AlphabetHelper lettersHelper = new AlphabetHelper();
+
+            char[] messageArray = activity.Text.ToCharArray();
+
+            foreach (char item in messageArray)
+            {
+                string finalCharacter = item.ToString().ToLower();
+                await context.PostAsync(lettersHelper.GetLetterName(finalCharacter));
+            }
+            context.Wait(MessageReceivedAsync);
+        }
+    }
+}
---
+++ CSharp/BotFonetico/BotFonetico/Helpers/AlphabetHelper.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BotFonetico.Helpers
+{
+    public class AlphabetHelper
+    {
+        private Dictionary<string, string> alphabetDictionary;
+
+        private void PopulateDictionary()
+        {
+            alphabetDictionary = new Dictionary<string, string>();
+
+            alphabetDictionary.Add("a", "alfa");
+            alphabetDictionary.Add("b", "bravo");
+            alphabetDictionary.Add("c", "coca");
+            alphabetDictionary.Add("d", "delta");
+            alphabetDictionary.Add("e", "eco");
+            alphabetDictionary.Add("f", "foxtrot");
+            alphabetDictionary.Add("g", "golfo");
+            alphabetDictionary.Add("h", "hotel");
+            alphabetDictionary.Add("i", "india");
+            alphabetDictionary.Add("j", "julieta");
+            alphabetDictionary.Add("k", "kilo");
+            alphabetDictionary.Add("l", "lima");
+            alphabetDictionary.Add("m", "metro");
+            alphabetDictionary.Add("n", "noviembre");
+            alphabetDictionary.Add("o", "oscar");
+            alphabetDictionary.Add("p", "papa");
+            alphabetDictionary.Add("q", "quebec");
+            alphabetDictionary.Add("r", "romeo");
+            alphabetDictionary.Add("s", "sierra");
+            alphabetDictionary.Add("t", "tango");
+            alphabetDictionary.Add("u", "unico");
+            alphabetDictionary.Add("v", "victor");
+            alphabetDictionary.Add("w", "wiskey");
+            alphabetDictionary.Add("x", "extra");
+            alphabetDictionary.Add("y", "yanke");
+            alphabetDictionary.Add("z", "zulu");
+        }
+
+        public string GetLetterName(string selectedLetter)
+        {
+            PopulateDictionary();
+
+            var theValue = alphabetDictionary[selectedLetter];
+            return theValue;
+        }
+
+        
+    }
+}
